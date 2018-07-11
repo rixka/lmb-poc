81a6669c056b51608a9a69b7d649bdbf9fde8e53
Kayleigh Rix <rixka@outlook.com>
2018-07-11 17:56:44 +0100

    [add] functionality for `/cupcakes`
    This also includes some very basic pagination using the objectId and limiting the number of documents returned at a time. Expected behaviour is to pass in the query string `last-id=xxxxx` and it will return the next 5 documents.
    
    Ideally this would be implemnented with a cursor and include proper page info in the request:
    
    ```
    {
      "data": {
        "hero": {
          "name": "R2-D2",
          "friendsConnection": {
            "totalCount": 3,
            "edges": [
              {
                "node": {
                  "name": "Han Solo"
                },
                "cursor": "Y3Vyc29yMg=="
              },
              {
                "node": {
                  "name": "Leia Organa"
                },
                "cursor": "Y3Vyc29yMw=="
              }
            ],
            "pageInfo": {
              "endCursor": "Y3Vyc29yMw==",
              "hasNextPage": false
            }
          }
        }
      }
    }
    ```

--- app/api.py
+++ app/api.py
@@ -1,7 +1,17 @@
+from os import environ
 from flask import Blueprint, abort
-from utils import json_response
+from pymongo import MongoClient
+
+from utils import (
+    json_response, validate_object_id,
+    parse_query
+)
+
+MONGO_HOST = environ.get('MONGO_HOST') or 'localhost'
+MONGO_DB = environ.get('MONGO_DB') or 'development'
 
 api = Blueprint('api', __name__)
+db = MongoClient(MONGO_HOST, 27017)[MONGO_DB]
 
 
 @api.route('/', defaults={'path': ''})
@@ -13,6 +23,15 @@ def catch_all(path):
 def health():
     return json_response({'status': 'ok'}, 200)
 
+@api.route('/cupcakes')
+def cupcakes_list():
+    last_id =  parse_query('last-id')
+    query = { '_id': { '$gt': validate_object_id(last_id) } } if last_id else {}
+
+    cupcakes = db.cupcakes.find(query).sort('_id').limit(5)
+    return json_response({
+        'data': cupcakes
+    })
 
 # === HANDLERS === #
 
